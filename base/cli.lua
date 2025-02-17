2e0800b0004e2f52bb16c2479a326fcacf5a5d87
viegg69 <vinagg69@gmail.com>
2025-02-18 06:03:16 +0700

    Update

--- base/cli.lua
+++ base/cli.lua
@@ -1,15 +1,17 @@
+local _toast = gg and gg.toast or print
+local _sleep = gg and gg.sleep or function(...) end
 local cli = {
     count = 0,
     name = '',
     toast = function(self, index, size, text, sleep)
         if type(index) == "string" and type(size) == "string" and not text then
-            gg.toast(string.format("%s\n\n\t%s:\n\t%s\n\n", self.name, size, index))
-            gg.sleep(sleep or 20)
+            _toast(string.format("%s\n\n\t%s:\n\t%s\n\n", self.name, size, index))
+            _sleep(sleep or 20)
         else
             if self:pt(index, size) then
                 local count = self.count * (size > 100 and 1 or 10)
-                gg.toast(text and string.format("%s\n\n\t%s %d%%\n\n", self.name, text, count) or count .. "%")
-                gg.sleep(sleep or 20)
+                _toast(text and string.format("%s\n\n\t%s %d%%\n\n", self.name, text, count) or count .. "%")
+                _sleep(sleep or 20)
             else
                 return false
             end
@@ -26,4 +28,4 @@ local cli = {
     end,
 }
 
-return gg and cli or {error='not support cli'}
+return cli
